0f4d0a4660a0c32ae273b6ae8fe756cd64c92260
Anatolii_Stoliarov <anatolii_stoliarov@epam.com>
2021-01-14 16:40:31 +0200

    Add deletion option for ebs volumes.

--- workers_launch_template.tf
+++ workers_launch_template.tf
@@ -360,7 +360,11 @@ resource "aws_launch_template" "workers_launch_template" {
         "root_kms_key_id",
         local.workers_group_defaults["root_kms_key_id"],
       )
-      delete_on_termination = true
+      delete_on_termination = lookup(
+        var.worker_groups_launch_template[count.index],
+        "ebs_delete",
+        local.workers_group_defaults["ebs_delete"],
+      )
     }
   }
 
